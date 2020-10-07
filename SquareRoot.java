97c82c5fd81c40e10e910a0ac92b17fe2ab325d8
JordanNicholson <71711661+JordanNicholson@users.noreply.github.com>
2020-10-07 13:24:54 -0400

    Added while statement

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,7 +1,7 @@
 /*
- * Programmer:
- * Date:
- * Purpose: 
+ * Programmer: Jordan Nicholson
+ * Date: October 7, 2020
+ * Purpose: To tell the user the square root of a number.
  */
 
 import java.text.DecimalFormat;
@@ -9,7 +9,8 @@ import java.util.Scanner;
 
 public class SquareRoot {
     public static void main (String [] args) {
-        // Create the input scanner
+       
+    	// Create the input scanner
         Scanner input = new Scanner(System.in);
 
         // Ask the user for the diameter
@@ -18,9 +19,10 @@ public class SquareRoot {
 
         // Check that the input is not negative
         // TODO: Change this part of the program to repeatedly error check
-        if (radicand < 0) {
+        while (radicand < 0) {
             System.err.println("Invalid input!");
-            System.exit(1);
+            System.err.print("Enter a number to square root: ");
+            radicand = input.nextDouble();
         }
 
         // Calculate the circumference
